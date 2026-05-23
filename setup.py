65a0c2102032c390441675f5106b17858e242fd5
Shah Sawar <shahsawar.officials@gmail.com>
2026-05-23 20:19:47 +0500

    initilize git init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='i am learning mlops from campus x',
+    author='ShahSawar',
+    license='MIT',
+)
